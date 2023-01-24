903ed5400f2689e891fa05c1d70969c8469bcc2d
dekde42 <dekde42@gmail.com>
2023-01-24 23:55:27 +0700

    functions

--- js/cards.js
+++ js/cards.js
@@ -1,7 +1,7 @@
 //Producto a favoritos
-$('.card .aFavs').click(function(){
-    $(this).parents('.card').toggleClass('esFav');
-  })
+// $('.card .aFavs').click(function(){
+//     $(this).parents('.card').toggleClass('esFav');
+//   })
   //Producto al carrito
   $('.card .alCarrito').click(function(){
     $(this).parents('.card').toggleClass('enCarrito');
